5572632b242d17921bb3108c4a91db4ba8ed2ee9
ggomi <dhkdn4042@naver.com>
2018-11-13 09:15:24 +0900

    Init Project

---
+++ src/main/java/com/ggomi/whereparcel/WhereparcelApplication.java
@@ -0,0 +1,12 @@
+package com.ggomi.whereparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereparcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereparcelApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ggomi/whereparcel/WhereparcelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ggomi.whereparcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhereparcelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
